598bfd18eee65407a64a5c96330bc9a6b38e3682
Amour Omar <amourhamisiomar@gmail.com>
2025-05-04 18:56:37 +0300

    Type error fix

--- components/RecorderComponents/Recorder.tsx
+++ components/RecorderComponents/Recorder.tsx
@@ -1,26 +1,14 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import Textarea from "./Textarea/Textarea";
 import Selection from "./Selection/Selection";
 
 const Recorder: React.FC = () => {
-  const [isRecording, setIsRecording] = useState(false);
-
-  const handleStartRecording = () => {
-    setIsRecording(true);
-  };
-
   return (
     <div className="w-full flex flex-col gap-4">
-      <Textarea 
-        isRecording={isRecording}
-        onStartRecording={handleStartRecording}
-      />
-      <Selection 
-        isRecording={isRecording}
-        onStartRecording={handleStartRecording}
-      />
+      <Textarea />
+      <Selection />
     </div>
   );
 };
